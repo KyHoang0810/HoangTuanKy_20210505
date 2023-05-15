6a580c2d31105aeb4814eebdb37fe3adae49e7fc
KyHoang0810 <130714812+KyHoang0810@users.noreply.github.com>
2023-05-15 15:19:24 +0700

    Add files via upload

---
+++ lab_2/CartTest.java
@@ -0,0 +1,19 @@
+package lab_2;
+
+public class CartTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cart cart=new Cart();
+		DigitalVideoDisc dvd1=new DigitalVideoDisc("The Lion King","Animation","Roger Allers",87, 19.95f);
+		cart.addDigitalVideoDisc(dvd1);
+		DigitalVideoDisc dvd2=new DigitalVideoDisc("Star Wars","Scifi","George Lucas",87, 24.95f);
+		cart.addDigitalVideoDisc(dvd2);
+		DigitalVideoDisc dvd3=new DigitalVideoDisc("Aladdin","Animation",18.99f);
+		cart.addDigitalVideoDisc(dvd3);
+		
+		cart.print();
+
+	}
+
+}
