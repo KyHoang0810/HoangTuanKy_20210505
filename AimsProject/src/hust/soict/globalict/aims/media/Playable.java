0b01c5683763c859f3d5e76d9740aaf1aff92da4
PC <PC@LAPTOP-HT1A6PAT>
2023-06-21 20:42:17 +0700

    commit

--- AimsProject/src/hust/soict/globalict/aims/media/Playable.java
+++ AimsProject/src/hust/soict/globalict/aims/media/Playable.java
@@ -3,7 +3,7 @@ package hust.soict.globalict.aims.media;
 import hust.soict.globalict.aims.exception.PlayerException;
 import java.lang.Exception;
 
-public interface Playable throws PlayerException{
+public interface Playable{
 	public void play() throws PlayerException;
 
 }
